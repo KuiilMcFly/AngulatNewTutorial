ac738ab3e73b6ac309e90593ff458968f793d41f
Carmine Esposito <105643457+KuiilMcFly@users.noreply.github.com>
2023-06-12 09:48:30 +0200

    Angular Tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ProvaComponent } from './prova/prova.component';
+import { BannerComponent } from './banner/banner.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSliderModule} from '@angular/material/slider'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProvaComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
